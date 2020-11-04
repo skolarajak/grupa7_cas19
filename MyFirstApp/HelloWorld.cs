58e6d2c9d57b5176e43ef144d5f6817be0200471
Skola Rajak QA <skolarajak.qa@gmail.com>
2020-11-04 21:55:30 +0100

    Add project files.

---
+++ MyFirstApp/HelloWorld.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace MyFirstApp
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            // Ovo je komentar
+            Console.WriteLine("Hello world."); // I ovo je komentar
+            /*
+             * 
+             * Ovo je komentar
+             * iz vise redova
+             * 
+             */
+            Console.WriteLine("");
+
+            double d = 2.3, dd = 4.6;
+            float f = 123.45F;
+
+            int i, j, k, l;
+
+            Console.WriteLine(dd);
+            Console.WriteLine(f);
+
+            i = (int)d;
+            f = i;
+
+            string s = "123";
+            string s2 = "Ovo je dodatni string.";
+            string s3;
+
+            s3 = Convert.ToString(Convert.ToInt32(s) + i);
+
+            Console.WriteLine(s3);
+
+            Console.WriteLine(i);
+            Console.WriteLine(f);
+
+            Console.ReadKey();
+        }
+    }
+}
